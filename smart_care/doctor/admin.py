18e545a7e4b254754f008196cc721bba5ad811a5
Shazzadul Islam Chowdhury <sazzadchowdhury87@gmail.com>
2026-04-30 23:09:00 +0600

    Doctor Model added

--- smart_care/doctor/admin.py
+++ smart_care/doctor/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from  .import models
 
 # Register your models here.
+class specializationAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+class designationAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+class DoctorAdmin (admin.ModelAdmin):
+    list_display = ('name', 'fees')
+
+    def name(self, obj):
+        return obj.user.get_full_name()
+
+
+
+admin.site.register(models.AvailableTime)
+admin.site.register(models.Specialization,specializationAdmin)
+admin.site.register(models.Designation,designationAdmin)
+admin.site.register(models.Doctor,DoctorAdmin)
+
+
+
--- smart_care/doctor/models.py
+++ smart_care/doctor/models.py
@@ -1,3 +1,36 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Specialization(models.Model):
+    name = models.CharField(max_length=30)
+    slug = models.SlugField(max_length=40)
+    
+    def __str__(self):
+        return self.name
+    
+class Designation(models.Model):
+    name = models.CharField(max_length=30)
+    slug = models.SlugField(max_length=40)
+    
+    def __str__(self):
+        return self.name
+    
+
+class AvailableTime(models.Model):
+    name = models.CharField(max_length=30)
+    def __str__(self):
+        return self.name
+    
+
+class Doctor(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='doctor/images/')
+    specialization= models.ManyToManyField(Specialization)
+    designation= models.ManyToManyField(Designation)
+    available_time= models.ManyToManyField(AvailableTime)
+    fees = models.IntegerField()
+    meet_link = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return f"{self.user.first_name} {self.user.last_name} "
