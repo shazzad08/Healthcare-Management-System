26689eb12ab9895711748c1a6f00190e2900891c
Shazzadul Islam Chowdhury <sazzadchowdhury87@gmail.com>
2026-05-07 23:49:43 +0600

    Built Appointment API

--- smart_care/appointment/models.py
+++ smart_care/appointment/models.py
@@ -17,7 +17,7 @@ class Appointment(models.Model):
     patient = models.ForeignKey(Patient,on_delete=models.CASCADE)
     doctor = models.ForeignKey(Doctor,on_delete=models.CASCADE)
     symptom = models.TextField()
-    time = models.OneToOneField(AvailableTime,on_delete=models.CASCADE)
+    time = models.ForeignKey(AvailableTime,on_delete=models.CASCADE)
     cancel= models.BooleanField(default=False)
     appointment_types= models.CharField(choices=APPOINTMENT_TYPES,max_length=30)
     appointment_status= models.CharField(choices= APPOINTMENT_STATUS,max_length=30,default='pending')
