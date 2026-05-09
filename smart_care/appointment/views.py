dfa433ac3eb52f93b6d6ec03b4db514bbf2accf2
Shazzadul Islam Chowdhury <sazzadchowdhury87@gmail.com>
2026-05-09 20:25:36 +0600

    add email feature in appointment app

--- smart_care/appointment/views.py
+++ smart_care/appointment/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .import models
 from .import serializers
+
 # Create your views here.
 
 class appointmentViewset(viewsets.ModelViewSet):
