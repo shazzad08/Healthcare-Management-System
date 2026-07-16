fe83eadea8057a715da63932850b6c0fb08973d3
Shazzadul Islam Chowdhury <sazzadchowdhury87@gmail.com>
2026-07-17 00:32:41 +0600

    Fix homepage 404 error by adding root url and template dir in nested config

--- smart_care/smart_care/urls.py
+++ smart_care/smart_care/urls.py
@@ -18,7 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
+
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
     path('admin/', admin.site.urls),
     path('contact_us/', include('contact_us.urls')),
     path('service/', include('service.urls')),
